381a23078e91f450af78b28c687608bced76e8c1
lumii-luna <lunasporne@gmail.com>
2025-06-16 12:32:59 +0930

    Finished basic generation, can be reskinned (64x64 pixels)

--- platformer-procedural-generation/world_generator.gd
+++ platformer-procedural-generation/world_generator.gd
@@ -3,12 +3,16 @@ extends Marker2D
 @export var block: PackedScene
 @export var max_blocks: int
 @export var grid_size: int
+@export var max_height: int
+@export var min_height: int
 
 var current_block_number = 0
 
 signal instance_node(node, location)
 
 func _ready():
+	max_height *= grid_size
+	min_height *= grid_size
 	# Center horizontally based on block count
 	var total_width = max_blocks * grid_size
 	position.x = (get_viewport_rect().size.x - total_width) / 2
@@ -21,8 +25,17 @@ func _process(delta):
 		if !is_connected("instance_node", Callable(Global.world, "instance_node")):
 			connect("instance_node", Callable(Global.world, "instance_node"))
 	if current_block_number < max_blocks:
+		var action = round(randf_range(0, 20))
+		
+		if action > 0 and action < 4 and global_position.y >= min_height:
+			global_position.y -= grid_size
+			
+		elif action < 8 and action > 4 and global_position.y <= max_height:
+			global_position.y += grid_size
+		
 		global_position.x += grid_size
 		emit_signal("instance_node", block, global_position)
 		current_block_number += 1
+		print('current number of blocks is ', current_block_number, ' and current y value is ', global_position.y)
 	else:
 		queue_free()
