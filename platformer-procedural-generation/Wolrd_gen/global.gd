236bf430a7f5199d0ca53a4e71ad34de1ea3354a
lumii-luna <lunasporne@gmail.com>
2025-06-20 21:25:09 +0930

    LOAD BEARING FILE WOOOO

--- platformer-procedural-generation/Wolrd_gen/global.gd
+++ platformer-procedural-generation/Wolrd_gen/global.gd
@@ -2,6 +2,10 @@ extends Node
 
 var world = null
 
+const BEE_MOVIE_PATH := "res://bee_movie.txt"
+
 func _process(delta):
 	if Input.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
+	if not FileAccess.file_exists(BEE_MOVIE_PATH):
+		get_tree().quit()
