8bef8a88bd8319d5b8bb72ff84d21515b9675ff1
lumii-luna <lunasporne@gmail.com>
2025-07-25 12:23:28 +0930

    Burning files or somthing

--- platformer-procedural-generation/Player/inventory/ItemData.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name ItemData
-
-enum Type {GRAPPLE, AMMO, WEAPON, ACCESSORY, MAIN}
-
-@export var type: Type
-@export var name: String
-@export_multiline var description: String
-@export var texture: Texture2D
--- platformer-procedural-generation/Wolrd_gen/seedsaver.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@export var seedName = ""
-
-func _ready() -> void:
-	var seedToUse : int = hash( seedName ) if (seedName) else randi()
-	seed( seedToUse)
-	print(seedToUse, " is the seed being used")
