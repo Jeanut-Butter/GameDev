1f7faad85b1e931132c5d51e7e40ddbd19237d40
poohead383 <Jasper.seb.cox@gmail.com>
2025-08-01 22:05:32 +0930

    buggy but proof of conept

--- Latch Break/Wolrd_gen/Rooms/WORLD_GEN.gd
+++ Latch Break/Wolrd_gen/Rooms/WORLD_GEN.gd
@@ -3,39 +3,51 @@ extends Node2D
 @export var room_scenes: Array[PackedScene]
 @export var corridor_scene: PackedScene
 @export var start_room_scene: PackedScene
-var occupied_positions = {}
+
+const ROOM_SIZE = Vector2(512, 288)
+var occupied_positions: Dictionary = {}
 
 func _ready():
 	generate_level(Vector2i.ZERO)
-	
-func generate_level(start_pos: Vector2i):
-	var rooms_to_generate = 5
-	var current_pos = start_pos
 
-	place_room(start_room_scene, current_pos)
+func generate_level(start_pos: Vector2i):
+	var current_pos = start_pos
+	var rooms_to_generate = 50
+
+	place_room(start_room_scene, start_pos)
 
 	for i in range(rooms_to_generate):
-		var dir = [Vector2i.RIGHT, Vector2i.LEFT, Vector2i.UP, Vector2i.DOWN].pick_random()
+		var dir = pick_weighted_direction()
 		var next_pos = current_pos + dir
 
-		while occupied_positions.has(next_pos):
-			dir = [Vector2i.RIGHT, Vector2i.LEFT, Vector2i.UP, Vector2i.DOWN].pick_random()
-			next_pos = current_pos + dir
+		if occupied_positions.has(next_pos):
+			continue
 
 		var new_room = room_scenes.pick_random()
 		place_room(new_room, next_pos)
-		connect_rooms(current_pos, next_pos)
 		current_pos = next_pos
+
+func pick_weighted_direction() -> Vector2i:
+	var direction_pool = [
+		Vector2i.RIGHT, Vector2i.RIGHT, Vector2i.RIGHT,
+		Vector2i.LEFT, Vector2i.LEFT,
+		Vector2i.UP,
+		Vector2i.DOWN
+	]
+	return direction_pool.pick_random()
 
 func place_room(scene: PackedScene, grid_pos: Vector2i):
 	var room = scene.instantiate()
-	room.position = Vector2(grid_pos) * Vector2(512, 288)
+	room.position = Vector2(grid_pos) * ROOM_SIZE
 	add_child(room)
 	occupied_positions[grid_pos] = room
 
-func connect_rooms(pos1: Vector2i, pos2: Vector2i):
-	var corridor = corridor_scene.instantiate()
-	var offset = Vector2(pos2 - pos1) * Vector2(256, 144)
-	corridor.position = Vector2(pos1) * Vector2(512, 288) + offset
+	draw_debug_outline(room.position, Color.GREEN)
 
-	add_child(corridor)
+func draw_debug_outline(position: Vector2, color: Color):
+	var rect = ColorRect.new()
+	rect.size = ROOM_SIZE
+	rect.position = position
+	rect.color = color
+	rect.modulate.a = 0.25
+	add_child(rect)
